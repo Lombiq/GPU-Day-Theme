dea52be1241607502449f3ca872887e914524b7a
Gábor Domonkos <gabor.domonkos@lombiq.com>
2017-04-19 16:45:27 +0200

    Initial commit with skeleton

---
+++ ResourceManifest.cs
@@ -0,0 +1,12 @@
+﻿using Orchard.UI.Resources;
+
+namespace Bismarq.Frontend
+{
+    public class ResourceManifest : IResourceManifestProvider
+    {
+        public void BuildManifests(ResourceManifestBuilder builder)
+        {
+            var manifest = builder.Add();
+        }
+    }
+}
