facfce08639fc5a925275f31b9bc6088eabf9e8f
Gábor Domonkos <gabor.domonkos@lombiq.com>
2017-04-19 17:38:37 +0200

    Removing cshtml files from Views folder, smaller changes

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Lombiq.PrettyGoodBootstrapBaseTheme")]
+[assembly: AssemblyTitle("GPUDayTheme")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Lombiq.PrettyGoodBootstrapBaseTheme")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyProduct("GPUDayTheme")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
