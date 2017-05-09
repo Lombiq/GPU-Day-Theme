53d2f3cee4c79924ef0c9bfcca4dada1ad086b47
Gábor Domonkos <gabor.domonkos@lombiq.com>
2017-05-09 02:17:13 +0200

    Earning the "Gulp master" badge
    --HG--
    branch : feature/SITE-7

--- gulpfile.js
+++ gulpfile.js
@@ -1,183 +1,77 @@
 // Simple Tempate - Made By Eon - All rights reserved
-var gulp = require('gulp');
-var browserSync = require('browser-sync').create();
+const gulp = require("gulp");
+const uglify = require("gulp-uglify");
+const rename = require("gulp-rename");
+const watch = require("gulp-watch");
+const batch = require("gulp-batch");
 
 var plugins = require("gulp-load-plugins")({
-    pattern: ['gulp-*', 'gulp.*'],
+    pattern: ["gulp-*", "gulp.*"],
     replaceSting: /\bgulp[\-.]/
 });
 
-var reload  = browserSync.reload;
 
-// Js
-// Concat + Minifiy Js Files and move to vendor folder
-var jsFiles = ['Content/SimpleTemplate/js/plugins/*.js'];
-var jsDest = 'Scripts/';
-
-gulp.task('js', function () {
-    return gulp.src(jsFiles)
-        .pipe(plugins.order([
-            'jquery.min.js',
-            'bootstrap.min.js',
-            'imagesloaded.pkgd.min.js',
-            '*.js',
-        ]))
-        .pipe(plugins.concat('plugins.js'))
-        .pipe(gulp.dest(jsDest))
-        .pipe(plugins.rename({ suffix:'.min' }))
+// JS compilation
+gulp.task("js-min-plugins", function () {
+    return gulp.src([
+        "Content/SimpleTemplate/js/modernizr.js",
+        "Content/SimpleTemplate/js/plugins/owl.carousel.min.js"])
+        .pipe(plugins.concat("plugins.js"))
+        .pipe(plugins.rename({ suffix: ".min" }))
         .pipe(plugins.uglify())
-        .pipe(gulp.dest(jsDest));
+        .pipe(gulp.dest("Scripts/"));
 });
 
-// Concat + Minify modernizer.js file
-var jsFiles = 'Content/SimpleTemplate/js/modernizr.js';
-var jsDest = 'Scripts/';
-
-gulp.task('js-modernizer', function () {
-    return gulp.src(jsFiles)
-        .pipe(plugins.order([
-            'jquery.min.js',
-            'bootstrap.min.js',
-            'imagesloaded.pkgd.min.js',
-            '*.js',
-        ]))
-        .pipe(plugins.concat('plugins.js'))
-        .pipe(gulp.dest(jsDest))
-        .pipe(plugins.rename({ suffix: '.min' }))
+gulp.task("js-min-main", function () {
+    return gulp.src([
+        "Scripts/main.js"])
+        .pipe(plugins.rename({ suffix: ".min" }))
         .pipe(plugins.uglify())
-        .pipe(gulp.dest(jsDest));
+        .pipe(gulp.dest("Scripts/"));
 });
 
 // Css
 // Concat + Minifiy Css Files and move to vendor folder
-var cssFiles = 'Content/SimpleTemplate/css/plugins/*.css';
-var cssDest = 'Styles/css/';
-
-gulp.task('css', function () {
-    return gulp.src(cssFiles)
-        .pipe(plugins.order([
-            'bootstrap.css',
-            '*.css'
-        ]))
-        .pipe(plugins.concat('plugins.css'))
-        .pipe(gulp.dest(cssDest))
-        .pipe(plugins.rename({ suffix:'.min' }))
-        .pipe(plugins.cssmin({keepBreaks: true, keepSpecialComments : '*' }))
-        .pipe(gulp.dest(cssDest));
+gulp.task("css-min-plugins", function () {
+    return gulp.src([
+        "Content/SimpleTemplate/css/plugins/vspacing.min.css"])
+        .pipe(plugins.concat("plugins.css"))
+        .pipe(plugins.rename({ suffix: ".min" }))
+        .pipe(plugins.cssmin({ keepBreaks: true, keepSpecialComments: "*" }))
+        .pipe(gulp.dest("Styles/"));
 });
 
 // Sass
-var sassFile = 'Content/SimpleTemplate/sass/style.scss';
-var sassDest = 'Styles/scss/';
-gulp.task('sass', function () {
-    return gulp.src(sassFile)
-        .pipe(plugins.sass({outputStyle: 'expanded'})) // expanded - compressed - compact - nested
+gulp.task("sass-min-style", function () {
+    return gulp.src("Content/SimpleTemplate/sass/style.scss")
+        .pipe(plugins.sass({ outputStyle: "expanded" })) // expanded - compressed - compact - nested
         .pipe(plugins.autoprefixer({
-            browsers: ['last 2 versions', 'ie 9'],
+            browsers: ["last 2 versions", "ie 9"],
             cascade: false
         }))
-        .pipe(gulp.dest(sassDest));
+        .pipe(plugins.rename({ suffix: ".min" }))
+        .pipe(plugins.cssmin({ keepBreaks: true, keepSpecialComments: "*" }))
+        .pipe(gulp.dest("Styles/"));
 });
 
-// Custom Saas
-var sassFile = 'Styles/sass/general.scss';
-var sassDest = 'Styles/sass/';
-gulp.task('customSass', function () {
-    return gulp.src(sassFile)
-        .pipe(plugins.sass({ outputStyle: 'expanded' })) // expanded - compressed - compact - nested
-        .pipe(plugins.autoprefixer({
-            browsers: ['last 2 versions', 'ie 9'],
-            cascade: false
-        }))
-        .pipe(gulp.dest(sassDest));
-});
 
 // Sass index-event file
-var sassFile = 'Content/SimpleTemplate/sass/homepages/index-event.scss';
-var sassDest = 'Styles/sass/';
-gulp.task('sass-index-event', function () {
-    return gulp.src(sassFile)
-        .pipe(plugins.sass({ outputStyle: 'expanded' })) // expanded - compressed - compact - nested
+gulp.task("sass-index-event", function () {
+    return gulp.src("Content/SimpleTemplate/sass/homepages/index-event.scss")
+        .pipe(plugins.sass({ outputStyle: "expanded" })) // expanded - compressed - compact - nested
         .pipe(plugins.autoprefixer({
-            browsers: ['last 2 versions', 'ie 9'],
+            browsers: ["last 2 versions", "ie 9"],
             cascade: false
         }))
-        .pipe(gulp.dest(sassDest));
-});
-
-// Sass home pages
-gulp.task('sassHome', function () {
-    return gulp.src('Content/SimpleTemplate/sass/homepages/*.scss')
-        .pipe(plugins.sass({outputStyle: 'expanded'}))
-        .pipe(plugins.autoprefixer({
-            browsers: ['last 2 versions', 'ie 9'],
-            cascade: false
-        }))
-        .pipe(gulp.dest('Content/SimpleTemplate/css/homepages/'));
-});
-
-// Sass Skins files/folder
-gulp.task('skins', function () {
-    return gulp.src('Content/SimpleTemplate/sass/theme-skins/*.scss')
-        .pipe(plugins.sass({outputStyle: 'expanded'}))
-        .pipe(plugins.autoprefixer({
-            browsers: ['last 2 versions', 'ie 9'],
-            cascade: false
-        }))
-        .pipe(gulp.dest('Content/SimpleTemplate/css/theme-skins/'));
-});
-
-// call - gulp htmlmin
-// Html files minify 
-// this will make your html files minified 
-// make sure to copy all html files before using this
-gulp.task('htmlmin', function () {
-    return gulp.src('*.html')
-        .pipe(plugins.htmlmin({
-            collapseWhitespace: true,
-            removeComments: true,
-            minifyJS: true, // minify js too
-            minifyCSS: true // minify css too
-        }))
-        .pipe(gulp.dest(''))
-});
-
-// Images - Optimize jpeg and png images
-gulp.task('imagemin', function () {
-    return gulp.src('Content/SimpleTemplate/images/**/*')
-        .pipe(plugins.imagemin())
-        .pipe(gulp.dest('Content/SimpleTemplate/images'));
+        .pipe(plugins.rename({ suffix: ".min" }))
+        .pipe(plugins.cssmin({ keepBreaks: true, keepSpecialComments: "*" }))
+        .pipe(gulp.dest("Styles/"));
 });
 
 
-// This task will run css and js task 
-// To concat all plugins folder int plugins.css or plugins.js
-gulp.task('build', ['css', 'js'], function () {
-    console.log('Created plugins files.');
+// Watcher task.
+gulp.task("watch", function () {
+    watch("Scripts/main.js", batch(function (events, done) {
+        gulp.start("js-min-main", done);
+    }));
 });
-
-// Sync - Livereload
-gulp.task('browser-sync', ['sass', 'sassHome', 'skins'], function () {
-    // Change simple with your project's folder name
-    browserSync.init({
-        proxy: "localhost/simple/"
-    });
-
-    // watch scss files
-    gulp.watch(['Content/SimpleTemplate/sass/*.scss', 'Content/SimpleTemplate/sass/*/*.scss', '!Content/SimpleTemplate/theme-skins/'], ['sass', reload]);
-
-    // watch scss files
-    gulp.watch(['Content/SimpleTemplate/sass/homepages/*.scss'], ['sassHome', reload]);
-
-    //Watch skins scss files
-    gulp.watch(['Content/SimpleTemplate/sass/theme-skins/*.scss'], ['skins', reload]);
-
-    gulp.watch([
-        '*.html',
-        '*.php',
-        'Content/SimpleTemplate/js/main.js'
-    ]).on('change', reload);
-});
-
-// Default Task
-gulp.task('default', ['browser-sync']);
