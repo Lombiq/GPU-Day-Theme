2bec31ef83c5fedb5b268e78af37534f8ce5e9c9
Gábor Domonkos <gabor.domonkos@lombiq.com>
2017-05-25 18:13:10 +0200

    Showing the embed format of the recordings in a modal window

--- Scripts/main.js
+++ Scripts/main.js
@@ -15,6 +15,8 @@
 
             // Call Simple Functions
             this.checkMobile();
+            this.convertYouTubeUrlToEmbedMarkup();
+            this.stopYouTubeVideoOnModalHidden();
             this.mobileMenuDropdownFix();
             this.menuOnClick();
             this.scrollToTop();
@@ -34,14 +36,33 @@
                 this.mobile = false;
             }
         },
+        convertYouTubeUrlToEmbedMarkup: function() {
+            $(".event-part").find("#video").on("click", function () {
+                var youTubeUrl = $(this).data("id");
+                var modalWindowId = $(this).data("target");
+                modalWindowId = modalWindowId.substring(1, modalWindowId.length);
+
+                var regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
+                var match = youTubeUrl.match(regExp);
+
+                if (match && match[2].length == 11) {
+                    $("." + modalWindowId).find(".embed-responsive-item").prop("src", "https://www.youtube.com/embed/" + match[2]);
+                }
+            });
+        },
+        stopYouTubeVideoOnModalHidden: function () {
+            $(".embedYouTubeModal").on("hidden.bs.modal", function () {
+                $(this).find(".embed-responsive-item").prop("src", "");
+            });
+        },
         mobileMenuDropdownFix: function () {
-            if (typeof Modernizr === "object" && (Modernizr.mq('only all and (max-width: 767px)') || Modernizr.touchevents)) {
-                $('.navbar-nav').not('.nav-overlay').find('.dropdown-toggle').on('click', function (e) {
-                    var parent = $(this).closest('li');
+            if (typeof Modernizr === "object" && (Modernizr.mq("only all and (max-width: 767px)") || Modernizr.touchevents)) {
+                $(".navbar-nav").not(".nav-overlay").find(".dropdown-toggle").on("click", function (e) {
+                    var parent = $(this).closest("li");
                     // close all the siblings and their children
-                    parent.siblings().removeClass('open').find('li').removeClass('open');
+                    parent.siblings().removeClass("open").find("li").removeClass("open");
                     // open which one is clicked
-                    parent.toggleClass('open');
+                    parent.toggleClass("open");
 
                     // prevent
                     e.preventDefault();
@@ -52,17 +73,17 @@
         menuOnClick: function () {
             var self = this;
             // Menu on click scroll animation for onepages
-            $('.onepage-nav').find('a').on('click', function (e) {
-                var target = $(this).attr('href');
-                if (target.indexOf('#') === -1 || !$(target).length) {
+            $(".onepage-nav").find("a").on("click", function (e) {
+                var target = $(this).attr("href");
+                if (target.indexOf("#") === -1 || !$(target).length) {
                     return;
                 }
 
                 var elem = $(target),
                     targetPos = elem.offset().top;
 
-                $('html, body').animate({
-                    'scrollTop': targetPos
+                $("html, body").animate({
+                    "scrollTop": targetPos
                 }, 1200);
                 e.preventDefault();
             });
@@ -70,12 +91,12 @@
         owlCarousels: function () {
 
             /* Clients/Partners Carousel */
-            $('.clients-carousel.owl-carousel').owlCarousel({
+            $(".clients-carousel.owl-carousel").owlCarousel({
                 loop: true,
                 margin: 20,
                 responsiveClass: true,
                 nav: false,
-                navText: ['<i class="fa fa-angle-left">', '<i class="fa fa-angle-right">'],
+                navText: ['<i class="fa fa-angle-left">", "<i class="fa fa-angle-right">'],
                 dots: true,
                 autoplay: true,
                 autoplayTimeout: 10000,
@@ -101,25 +122,25 @@
         },
         scrollBtnAppear: function () {
             if ($(window).scrollTop() >= 400) {
-                $('#scroll-top').addClass('fixed');
+                $("#scroll-top").addClass("fixed");
             } else {
-                $('#scroll-top').removeClass('fixed');
+                $("#scroll-top").removeClass("fixed");
             }
         },
         scrollToTop: function () {
-            $('#scroll-top').on('click', function (e) {
-                $('html, body').animate({
-                    'scrollTop': 0
+            $("#scroll-top").on("click", function (e) {
+                $("html, body").animate({
+                    "scrollTop": 0
                 }, 1200);
                 e.preventDefault();
             });
         },
         scrollAnimations: function () {
             /* Wowy Plugin */
-            if (typeof WOW === 'function') {
+            if (typeof WOW === "function") {
                 new WOW({
-                    boxClass: 'wow',      // default
-                    animateClass: 'animated', // default
+                    boxClass: "wow",      // default
+                    animateClass: "animated", // default
                     offset: 0          // default
                 }).init();
             }
@@ -133,12 +154,12 @@
     });
 
     // Load Event
-    $(window).on('load', function () {
+    $(window).on("load", function () {
         Simple.scrollBtnAppear();
     });
 
     // Scroll Event
-    $(window).on('scroll', function () {
+    $(window).on("scroll", function () {
         Simple.scrollBtnAppear();
     });
 
@@ -148,13 +169,13 @@
         // Map pin coordinates and content of pin box
         var locations = [
             [
-                '<address><strong>Address:</strong> 29-33, Konkoly-Thege Miklos u, 1121 - Budapest, Hungary</address>',
+                "<address><strong>Address:</strong> 29-33, Konkoly-Thege Miklos u, 1121 - Budapest, Hungary</address>",
                 47.487416,
                 18.954929
             ]
         ];
 
-        var map = new google.maps.Map(document.getElementById('map'), {
+        var map = new google.maps.Map(document.getElementById("map"), {
             zoom: 14,
             center: new google.maps.LatLng(47.487416, 18.954929), // Map Center coordinates
             scrollwheel: false,
@@ -173,7 +194,7 @@
                 animation: google.maps.Animation.DROP
             });
 
-            google.maps.event.addListener(marker, 'click', (function (marker, i) {
+            google.maps.event.addListener(marker, "click", (function (marker, i) {
                 return function () {
                     infowindow.setContent(locations[i][0]);
                     infowindow.open(map, marker);
